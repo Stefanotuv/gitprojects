891e4f82b8987b601743f30f1a685e7380a6f7b3
stefanotuv <ste.tuveri@gmail.com>
2018-10-21 01:31:04 +0100

    It does work on usinf JS file from http://127.0.0.1:8000/exercise/
    In the JS file there is the injection of code {{}} from template/views

--- st1/exercise/views.py
+++ st1/exercise/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.template import Context, loader
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("INDEX PAGE")
+
+def exercise(request):
+    # all the options below work. the diffence in the path would require to
+    # declare the different referetn DIR in settings-> TEMPLATES /> DIR
+    # return render(request, 'exercise/exercise.html')
+    # return render(request, 'exercise.html') - required BASE_DIR+'/exercise'
+    # return render_to_response('exercise/exercise.html')
+    # return render_to_response('exercise.html') - required BASE_DIR+'/exercise'
+
+    template = loader.get_template("exercise/exercise.html")
+
+    return HttpResponse(template.render())
+#     return HttpResponse("Exercise page from HttpResponse")
